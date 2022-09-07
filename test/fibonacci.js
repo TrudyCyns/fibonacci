2849b17b755020aa54ea739f593c5a4d55e546e6
TrudyCyns <gcyn51a@gmail.com>
2022-09-07 12:06:36 +0300

    Test Message Editing

--- test/fibonacci.js
+++ test/fibonacci.js
@@ -3,16 +3,16 @@ const { describe, it } = require('mocha');
 const fibonacci = require('../fibonacci');
 
 describe('Fibonacci Numbers', () => {
-  it('0', () => {
+  it('Index 0 => 0', () => {
     assert.equal(fibonacci(0), 0);
   });
-  it('1', () => {
+  it('Index 1 => 1', () => {
     assert.equal(fibonacci(1), 1);
   });
-  it('5', () => {
+  it('Index 5 => 5', () => {
     assert.equal(fibonacci(5), 5);
   });
-  it('10', () => {
+  it('Index 10 => 55', () => {
     assert.equal(fibonacci(10), 55);
   });
 });
